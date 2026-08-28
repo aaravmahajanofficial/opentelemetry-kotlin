c20160b86dac28a2e09b57373cbca080fce1099d
Aarav Mahajan <aaravmahajan2003@gmail.com>
2026-08-29 01:27:33 +0530

    chore: remove unnecessary whitespace
    Signed-off-by: Aarav Mahajan <aaravmahajan2003@gmail.com>

--- behavior/src/commonMain/kotlin/io/opentelemetry/kotlin/behavior/SamplerBehavior.kt
+++ behavior/src/commonMain/kotlin/io/opentelemetry/kotlin/behavior/SamplerBehavior.kt
@@ -8,9 +8,7 @@ sealed class SamplerBehavior : Behavior<SamplerBehavior> {
     override fun mergeWith(higher: SamplerBehavior): SamplerBehavior = higher
 
     data object AlwaysOn : SamplerBehavior()
-
     data object AlwaysOff : SamplerBehavior()
-
     data class TraceIdRatioBased(val ratio: Double? = null) : SamplerBehavior()
 
     data class ParentBased(
